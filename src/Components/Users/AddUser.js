9331011dc8138f0450c100f7ad391bdf09d72b3d
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-17 13:17:04 +0100

    css modules and styles applied to form

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
 
     const addUserHandler = (e) => {
@@ -9,15 +12,20 @@ const AddUser = (props) => {
     };
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
 
-            <label htmlFor="age">Age (years)</label>
-            <input id="age" type="number" />
+        <Card className={classes.input}>
 
-            <button type="submit">Add User</button>
-        </form>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+
+                <label htmlFor="age">Age (years)</label>
+                <input id="age" type="number" />
+
+                <button type="submit">Add User</button>
+            </form>
+
+        </Card>
 
     );
 
